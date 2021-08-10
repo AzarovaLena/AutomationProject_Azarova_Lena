6c566519a3134fed43f49136260e73e6b5249f47
AzarovaLena <azarova.l375@gmail.com>
2021-08-10 15:05:00 +0300

    Createt my first automation tests

---
+++ src/test/java/CoogleSearchTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CoogleSearchTest {
+    @Test
+    public void test1() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.by/");
+        driver.findElement(By.name("q")).sendKeys("Hello world", Keys.ENTER);
+        Assert.assertEquals(driver.findElement(By.xpath("(//h3)[1]")).getText(), "Hello, world! Карта для тех, кто летает | МТБанк");
+        driver.quit();
+    }
+
+    @Test
+    public void test2() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.by/");
+        driver.findElement(By.name("q")).sendKeys("//", Keys.ENTER);
+        Assert.assertEquals(driver.findElement(By.xpath("(//p)[1]")).getText(), "По запросу // ничего не найдено. ");
+        driver.quit();
+    }
+}
